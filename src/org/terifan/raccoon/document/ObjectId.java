e68c853fe7e99269521a9052913824897572a352
terifan <pinjata69@gmail.com>
2023-09-29 15:26:49 +0200

    update

--- src/org/terifan/raccoon/document/ObjectId.java
+++ src/org/terifan/raccoon/document/ObjectId.java
@@ -41,6 +41,10 @@ public final class ObjectId implements Serializable, Comparable<ObjectId>
 		final static int SESSION = PRNG.nextInt();
 		final static AtomicInteger SEQUENCE = new AtomicInteger(PRNG.nextInt());
 		final static char[] BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+		final static long[] SHIFT =
+		{
+			1, 62, 3844, 238328, 14776336, 916132832, 56800235584L, 3521614606208L
+		};
 	}
 
 
@@ -101,36 +105,51 @@ public final class ObjectId implements Serializable, Comparable<ObjectId>
 
 	public static class Instance
 	{
-		final char[][] encoder = new char[24][32];
+		final char[][] encoder = new char[24][62];
 		final int[][] decoder = new int[24][128];
-		final int[][] tab = new int[3][256];
+		final int[][] tweak = new int[2][3];
 
 
-		public Instance(long aSeed)
+		public Instance(long aSecret)
 		{
-			Random rnd = new Random(aSeed);
+			Random rnd = new Random(aSecret);
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					tweak[i][j] = rnd.nextInt();
+				}
+			}
 			for (int k = 0; k < 24; k++)
 			{
 				Arrays.fill(decoder[k], -1);
-				for (int i = 0; i < 32;)
+				int[] order = order(62, rnd);
+				for (int i = 0; i < 62;)
 				{
-					int j = rnd.nextInt(62);
-					if (decoder[k][Holder.BASE62[j]] == -1)
-					{
-						decoder[k][Holder.BASE62[j]] = i;
-						encoder[k][i++] = Holder.BASE62[j];
-					}
-				}
-			}
-
-			for (int j = 0; j < 3; j++)
-			{
-				for (int i = 0; i < 256; i++)
-				{
-					tab[j][i] = rnd.nextInt();
+					int j = order[i];
+					decoder[k][Holder.BASE62[j]] = i;
+					encoder[k][i++] = Holder.BASE62[j];
 				}
 			}
 		}
+	}
+
+
+	private static int[] order(int aLength, Random aRandom)
+	{
+		int[] order = new int[aLength];
+		for (int i = 0; i < aLength; i++)
+		{
+			order[i] = i;
+		}
+		for (int i = 0; i < aLength; i++)
+		{
+			int j = aRandom.nextInt(aLength);
+			int t = order[j];
+			order[j] = order[i];
+			order[i] = t;
+		}
+		return order;
 	}
 
 
@@ -140,19 +159,41 @@ public final class ObjectId implements Serializable, Comparable<ObjectId>
 	}
 
 
-	public String toArmoredString(Instance aInstance)
-	{
-		char[] buf = new char[24];
-		encode(aInstance, buf, 0, mTime);
-		encode(aInstance, buf, 1, mSession);
-		encode(aInstance, buf, 2, mSequence);
-		return new String(buf);
-	}
-
-
 	public static ObjectId fromArmoredString(String aName)
 	{
 		return fromArmoredString(STATIC_INSTANCE, aName);
+	}
+
+
+	public String toArmoredString(Instance aInstance)
+	{
+		int a = mTime;
+		int b = mSession;
+		int c = mSequence;
+
+		a ^= aInstance.tweak[0][0];
+		b ^= aInstance.tweak[0][1];
+		c ^= aInstance.tweak[0][2];
+		for (int i = 0; i < 3; i++)
+		{
+			c -= Integer.rotateLeft(a, 9) ^ Integer.rotateRight(b, 7);
+			b -= Integer.rotateLeft(c, 13) ^ Integer.rotateRight(a, 25);
+			a -= Integer.rotateLeft(b, 5) ^ Integer.rotateRight(c, 10);
+		}
+		a ^= aInstance.tweak[1][0];
+		b ^= aInstance.tweak[1][1];
+		c ^= aInstance.tweak[1][2];
+
+		long chk = 0xffffffffL & ((long)a + (long)b + (long)c);
+		long A = (a & 0xffffffffL) * 13 + (chk % 13);
+		long B = (b & 0xffffffffL) * 13 + (chk / 13 % 13);
+		long C = (c & 0xffffffffL) * 13 + (chk / 13 / 13 % 13);
+
+		StringBuilder buf = new StringBuilder();
+		encode(aInstance, buf, A, 0, 6);
+		encode(aInstance, buf, B, 6, 6);
+		encode(aInstance, buf, C, 12, 6);
+		return buf.toString();
 	}
 
 
@@ -160,47 +201,67 @@ public final class ObjectId implements Serializable, Comparable<ObjectId>
 	{
 		try
 		{
-			int a = decode(aInstance, aName, 0);
-			int b = decode(aInstance, aName, 1);
-			int c = decode(aInstance, aName, 2);
+			long A = decode(aInstance, aName, 0, 6);
+			long B = decode(aInstance, aName, 6, 6);
+			long C = decode(aInstance, aName, 12, 6);
+
+			int a = (int)(A / 13);
+			int b = (int)(B / 13);
+			int c = (int)(C / 13);
+
+			long chk = 0xffffffffL & ((long)a + (long)b + (long)c);
+			if (A % 13 != chk % 13 || B % 13 != chk / 13 % 13 || C % 13 != chk / 13 / 13 % 13)
+			{
+				return null;
+			}
+
+			a ^= aInstance.tweak[1][0];
+			b ^= aInstance.tweak[1][1];
+			c ^= aInstance.tweak[1][2];
+			for (int i = 0; i < 3; i++)
+			{
+				a += Integer.rotateLeft(b, 5) ^ Integer.rotateRight(c, 10);
+				b += Integer.rotateLeft(c, 13) ^ Integer.rotateRight(a, 25);
+				c += Integer.rotateLeft(a, 9) ^ Integer.rotateRight(b, 7);
+			}
+			a ^= aInstance.tweak[0][0];
+			b ^= aInstance.tweak[0][1];
+			c ^= aInstance.tweak[0][2];
 
 			return new ObjectId(a, b, c);
 		}
-		catch (IllegalArgumentException e)
+		catch (Exception e)
 		{
 			return null;
 		}
 	}
 
 
-	private void encode(Instance aInstance, char[] aBuffer, int aIndex, long aValue)
+	private void encode(Instance aInstance, StringBuilder aOutput, long aValue, int aIndex, int aLength)
 	{
-		int z = new Random().nextInt(256);
-		aValue = ((aInstance.tab[aIndex][z] ^ aValue) << 8) | z;
-
-		for (int i = 35, j = 8 * aIndex; i >= 0; i -= 5, j++)
+		for (int i = 0, j = aIndex; i < aLength; i++, j++)
 		{
-			int symbol = (int)(31 & (aValue >>> i));
-			aBuffer[j] = aInstance.encoder[j][symbol];
+			int symbol = (int)(aValue % 62);
+			aOutput.append(aInstance.encoder[j][symbol]);
+			aValue /= 62;
 		}
 	}
 
 
-	private static int decode(Instance aInstance, String aName, int aIndex)
+	private static long decode(Instance aInstance, String aName, int aIndex, int aLength)
 	{
 		long value = 0;
-		for (int i = 35, j = 8 * aIndex; i >= 0; i -= 5, j++)
+		for (int i = 0, j = aIndex; i < aLength; i++, j++)
 		{
 			int symbol = aInstance.decoder[j][aName.charAt(j)];
 			if (symbol == -1)
 			{
 				throw new IllegalArgumentException();
 			}
-			value |= ((long)symbol) << i;
+			value += symbol * Holder.SHIFT[i];
 		}
 
-		int z = (int)(value & 0xff);
-		return (int)((value >>> 8) ^ aInstance.tab[aIndex][z]);
+		return value;
 	}
 
 
@@ -291,14 +352,25 @@ public final class ObjectId implements Serializable, Comparable<ObjectId>
 //			}
 //			System.out.println(System.currentTimeMillis() - t);
 
-			Instance instance = new Instance(System.currentTimeMillis());
+			Instance instance = new Instance(321954098761L);
+
 			for (int i = 0; i < 100; i++)
 			{
 				ObjectId in = ObjectId.randomId();
 				String encoded = in.toArmoredString(instance);
 				ObjectId out = ObjectId.fromArmoredString(instance, encoded);
 				System.out.printf("%s  %s  %s  %s%n", encoded, in, out == null ? "-".repeat(24) : out, in.equals(out));
+				if (!in.equals(out)) throw new IllegalStateException();
 			}
+
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf0"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf1"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf2"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf3"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf4"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf5"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf6"));
+//			System.out.println(ObjectId.fromArmoredString(instance, "UNR9gIdZObndSWNQ1lDoIcf7"));
 		}
 		catch (Throwable e)
 		{
