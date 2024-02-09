03ffd9a179875da60112a57cf54562c09314c9d8
terifan <pinjata69@gmail.com>
2024-02-09 16:40:54 +0100

    update

---
+++ src/test_document/Test.java
@@ -0,0 +1,18 @@
+package test_document;
+
+import org.terifan.raccoon.document.Document;
+
+
+public class Test
+{
+	public static void main(String ... args)
+	{
+		try
+		{
+		}
+		catch (Throwable e)
+		{
+			e.printStackTrace(System.out);
+		}
+	}
+}
